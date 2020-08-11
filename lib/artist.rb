1f346f16e1b035d1b674e9643e17045dbf3323d3
Anthony Lepore <violacellonyc@gmail.com>
2020-08-11 14:20:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Artist 
   attr_accessor :name 
   @@all = [] 
@@ -20,6 +21,7 @@ end
   
   def self.find_or_create_by_name(name)
     @@all.each do |i|
+      binding.pry 
       if i == self then return self 
     else
       @name = name 
