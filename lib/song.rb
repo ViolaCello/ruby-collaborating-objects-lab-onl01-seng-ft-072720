4da9bc80adfa31f0d5e9b23533c36c68523c399f
Anthony Lepore <violacellonyc@gmail.com>
2020-08-11 14:47:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
   def self.new_by_filename(filename)
     song = self.new
     song.name = filename.split(" - ")[1]
-    song
+    song.artist = filename.split(" - ")[0]
   end
     
   
