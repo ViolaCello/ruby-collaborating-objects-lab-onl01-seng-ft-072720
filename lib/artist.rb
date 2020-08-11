5c3574af1d89cc55567d893a44bf84bce217033c
Anthony Lepore <violacellonyc@gmail.com>
2020-08-11 16:42:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -46,13 +46,24 @@ class Artist
     @@all.push(self)
   end
   
-    def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
+   # def self.find_or_create_by_name(name)
+   # if self.find(name)
+  #    self.find(name)
+  #  else
+  #    self.create(name)
+  #  end
+#  end
+  
+  
+  def self.find_or_create_by_name(name)
+        if @@all.detect{|i| i.name == name}
+          @@all.detect{|i| i.name == name}
     else
-      self.create(name)
+      self.new(name)
+      # self.name
     end
   end
+  
   
   def self.find(name)
     self.all.detect { |artist| artist.name == name}
